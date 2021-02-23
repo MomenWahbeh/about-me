1ed0ff624c44857d9b3ff6adf791730a5aade3a6
momenwahbeh <Momen.wahbeh@hotmail.com>
2021-02-23 16:49:27 +0200

    lab2

---
+++ js/app.js
@@ -0,0 +1,50 @@
+'use strict';
+let user = prompt (" what\'s your name?")
+ 
+alert(user + "  let\'s play a Guessing Game ")
+
+
+let major = prompt("Did I graduated in MIS?")
+if(major == "yes" .toUpperCase()|| major =="y".toUpperCase()) {
+    alert ("Great!")
+}else if (major == "no".toUpperCase() || major =="n".toUpperCase()){
+    alert("Sorry Wrong Answer!")
+}else {
+    alert ("Enter only yes or no")
+}
+
+let uni = prompt("Did I graduated from the UJ")
+if(uni == "yes".toUpperCase()|| uni =="y".toUpperCase()) {
+    alert ("Great!")
+}else if (muni == "no".toUpperCase() || uni=="n".toUpperCase()){
+    alert("Sorry Wrong Answer!")
+}else {
+    alert ("Enter only yes or no")
+}
+
+
+let YoB = prompt("Did I born  in 1998")
+if(YoB == "yes".toUpperCase()|| YoB =="y".toUpperCase()) {
+    alert ("Sorry Wrong Answer!")
+}else if (YoB == "no".toUpperCase() || YoB =="n".toUpperCase()){
+    alert("Correct Answer!")
+}else {
+    alert ("Enter only yes or no")
+}
+let car = prompt("Do I have a car?")
+if(mcar == "yes".toUpperCase()|| car =="y".toUpperCase()) {
+    alert ("Great!")
+}else if (car == "no".toUpperCase() || car =="n".toUpperCase()){
+    alert("Sorry Wrong Answer!")
+}else {
+    alert ("Enter only yes or no")
+}
+let nationality = prompt("Am I American?")
+if(major == "yes".toUpperCase()|| major =="y".toUpperCase()) {
+    alert ("Hopefully")
+}else if (major == "no" .toUpperCase()|| major =="n".toUpperCase()){
+    alert("Great!")
+}else {
+    alert ("Enter only yes or no")
+}
+
