6d7c23c9608cfd783e7dd6fab20e31bb57b3dcf7
momenwahbeh <Momen.wahbeh@hotmail.com>
2021-02-23 22:10:56 +0200

    lab 3

--- js/app.js
+++ js/app.js
@@ -1,50 +1,93 @@
 'use strict';
 let user = prompt (" what\'s your name?")
  
-alert(user + "  let\'s play a Guessing Game ")
+alert("Welcome " + user + "  let\'s play a Guessing Game ")
 
 
 let major = prompt("Did I graduated in MIS?")
-if(major == "yes" .toUpperCase()|| major =="y".toUpperCase()) {
+if(major ==="yes" || major ==="y") {
     alert ("Great!")
-}else if (major == "no".toUpperCase() || major =="n".toUpperCase()){
+}else if (major === "no" || major==="n"){
     alert("Sorry Wrong Answer!")
 }else {
     alert ("Enter only yes or no")
 }
 
 let uni = prompt("Did I graduated from the UJ")
-if(uni == "yes".toUpperCase()|| uni =="y".toUpperCase()) {
+if(uni == "yes"|| uni =="y") {
     alert ("Great!")
-}else if (muni == "no".toUpperCase() || uni=="n".toUpperCase()){
+}else if (uni === "no" || uni==="n"){
     alert("Sorry Wrong Answer!")
 }else {
     alert ("Enter only yes or no")
 }
 
-
 let YoB = prompt("Did I born  in 1998")
-if(YoB == "yes".toUpperCase()|| YoB =="y".toUpperCase()) {
+if(YoB === "yes"|| YoB ==="y") {
     alert ("Sorry Wrong Answer!")
-}else if (YoB == "no".toUpperCase() || YoB =="n".toUpperCase()){
+}else if (YoB == "no" || YoB =="n"){
     alert("Correct Answer!")
 }else {
     alert ("Enter only yes or no")
 }
 let car = prompt("Do I have a car?")
-if(mcar == "yes".toUpperCase()|| car =="y".toUpperCase()) {
+if(car == "yes"|| car =="y") {
     alert ("Great!")
-}else if (car == "no".toUpperCase() || car =="n".toUpperCase()){
+}else if (car == "no" || car =="n"){
     alert("Sorry Wrong Answer!")
 }else {
     alert ("Enter only yes or no")
 }
-let nationality = prompt("Am I American?")
-if(major == "yes".toUpperCase()|| major =="y".toUpperCase()) {
-    alert ("Hopefully")
-}else if (major == "no" .toUpperCase()|| major =="n".toUpperCase()){
-    alert("Great!")
+let Nat = prompt("Am I Jordanian?")
+if(Nat == "yes"|| Nat =="y") {
+    alert ("Great!")
+}else if (Nat == "no" || Nat =="n"){
+    alert("Sorry Wrong Answer!")
 }else {
     alert ("Enter only yes or no")
+}
+
+let number = 7;
+for (let i = 0; i < 5; i++) {
+    let Attempts = prompt('guess a number from 1 to 15');
+    if (Attempts == number) {
+        alert('Good ' + 'the number is ' + number);
+        counter++;
+        break;
+    }
+    else if (Attempts > 7) {
+        alert('too high');
+    }
+    else {
+        alert('too low');
+    }
+    if (i == 4) {
+        alert('you can not try any more');
+    }
+
+};
+
+let Cars = ['Mercidis', 'BMW', 'Toyouta', 'Ople', 'Ford']
+let correct = false;
+
+for (let i = 0; i < 6; i++) {
+
+    let myCar = prompt('what is the brands of cars I like ?');
+    for (let j=0 ; j< Cars.length ; j++){
+        if (myCar == Cars [j]) {
+            alert('great'); 
+            correct = true ;
+            break ; 
+        }
+    }
+    if (correct) {
+        break ;
+    }
+
+}
+alert('I like the following cars brands ' + Cars );
+
+if (counter > 5) {
+    alert('your score is'   + counter + 'from 5');
 }
 
